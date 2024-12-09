3a66d9c1053aa129c71590c523419f8f7121d74c
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-12-10 00:08:59 +0530

    feature: notice & navbar created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,30 @@
 import brandLogo from "/brandLogo.svg";
-const Navbar:React.FC = () => {
-  return (
-    <>
-    <div className="">
-        <div className="">
-                  <img src={brandLogo} alt="brandLogo" />
-        </div>
-        <div className="">
-            <ul>
-                <li>Find Doctors</li>
-                <li>Hospitals</li>
-                <li>Medicines</li>
-                <li>Surgeires</li>
-                <li>Software for Provider</li>
-                <li>Facilities</li>
-                <li>My Bookings</li>
-            </ul>
-        </div>
-    </div>
-    </>
-  )
+const Navbar: React.FC = () => {
+    return (
+        <>
+            <div className="navbar">
+                <div className="nav-left-section">
+                    <img src={brandLogo} alt="brandLogo" />
+                </div>
+                <div className="nav-right-section">
+                    <ul className="nav-list">
+                        <li>Find Doctors</li>
+                        <li>Hospitals</li>
+                        <li>Medicines</li>
+                        <li>Surgeires</li>
+                        <li>Software for Provider</li>
+                        <li>Facilities</li>
+                        <li >
+                            <span className="my-booking-btn">
+
+                                My Bookings
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
