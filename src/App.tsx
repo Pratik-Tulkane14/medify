10457041487671479ff8d4b53c24431b38f3acf6
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-12-21 09:23:43 +0530

    feature: responsiveness added in blogs page

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       {/* <Hero /> */}
       {/* <Offers /> */}
       {/* <Specialisation /> */}
-      {/* <Blog /> */}
+      <Blog />
       <Families />
       {/* <MedicalSpecialist /> */}
       <PatientCaring />
